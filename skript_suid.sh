83204c6ab973a90b61256331609a05f0d5486187
Sebrekov <sebrekov@gmail.com>
2016-01-29 16:44:15 +0300

    Update skript_suid.sh

--- skript_suid.sh
+++ skript_suid.sh
@@ -1,1 +1,11 @@
 
+#!/bin/bash
+echo Начинаем процесс изменения прав
+chmod u-s /usr/bin/chfn
+chmod u-s /usr/bin/chsh
+chmod u-s /usr/bin/passwd
+setcap cap_chown,cap_setuid+ep /usr/bin/chfn
+setcap cap_chown,cap_setuid+ep /usr/bin/chsh
+setcap cap_chown,cap_dac_override,cap_fowner+ep /usr/bin/passwd
+
+echo ok
